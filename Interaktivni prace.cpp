a19da67541ac47d00ef7fd8c60533b3d495bba6e
Adam Elgheriani <adam.elgheriani.020@pslib.cz>
2021-11-07 14:26:46 +0100

    Hotová práce

--- Interaktivni prace.cpp
+++ Interaktivni prace.cpp
@@ -1,7 +1,8 @@
 ﻿#include <stdio.h>
+#include <math.h>
 
 int main() {
-	int vstup, a, b, c, max;
+	int vstup, a, b, c, p, max;
 	printf("Vyber si:\n");
 	printf("1) trojuhelnik\n");
 	printf("2) obdelnik\n");
@@ -15,6 +16,14 @@ int main() {
 		scanf("%d", &b);
 		printf("Zadej delku c:");
 		scanf("%d", &c);
+		//Existuje?
+		if ((a + b > c) && (b + c > a) && (c + a > b)) {
+			printf("Trojuhelnik existuje\n");
+		}
+		else {
+			printf("Trojuhelnik neexistuje\n");
+		}
+		//Nejdelsi strana
 		max = a;
 		if (b > max) {
 			max = b;
@@ -22,28 +31,60 @@ int main() {
 		if (c > max) {
 			max = c;
 		}
-		printf("Trojuhelnik:")                                                           //Nejdelsi strana je "max"
-		
+		printf("Nejdelsi strana je %d\n", max);
+		//Je pravouhly?
+		if (max == c) {
+			if (c * c == (a * a) + (b * b)) {
+				printf("Trojuhelnik je pravouhly\n");
+			}
+			else {
+				printf("Trojuhelnik neni pravouhly\n");
+			}
+		}
+		else if (max == b) {
+			if (b * b == (a * a) + (c * c)) {
+				printf("Trojuhelnik je pravouhly\n");
+			}
+			else {
+				printf("Trojuhelnik neni pravouhly\n");
+			}
+		}
+		else if (max == a) {
+			if (a * a == (b * b) + (c * c)) {
+				printf("Trojuhelnik je pravouhly\n");
+			}
+			else {
+				printf("Trojuhelnik neni pravouhly\n");
+			}
+		}
+		//Obsah, obvod
+		printf("Obvod trojuhelniku je %d\n", a + b + c);
+		p = ((a + b + c) / 2);
+		printf("Obsah trojuhelniku je %f", sqrt(p * (p - a) * (p - b) * (p - c)));
+
 	}
 	else if (vstup == 2) {
-		//resime obdelnik
+		//Resime obdelnik
 		printf("Zadej delku a:");
 		scanf("%d", &a);
 		printf("Zadej delku b:");
 		scanf("%d", &b);
+		//Je to obdelnik?
 		if (a == b) {
 			printf("Neni to obdelnik\n");
 		}
 		else {
 			printf("Je to obdelnik\n");
 		}
+		//Obvod, obsah
 		printf("O obvodu:%d\n", 2 * (a + b));
 		printf("A obsahu:%d\n", a * b);
 	}
 	else {
-		//resime kruznici
+		//Resime kruznici
 		printf("Zadej polomer: ");
 		scanf("%d", &a);
+		//Obvod, obsah
 		printf("Obvod: %f\n", 3.14 * 2 * a);
 		printf("Obsah: %f\n", 3.14 * a * a);
 	}
